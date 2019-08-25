9c2ab2eca8d6682d2a2f356105e42a1010592114
SerbanJinga <33983121+SerbanJinga@users.noreply.github.com>
2019-08-25 17:43:26 +0300

    Update Armstrong Number.cpp

--- Dynamic Programming/Armstrong Number.cpp
+++ Dynamic Programming/Armstrong Number.cpp
@@ -1,6 +1,8 @@
 //program to check whether a number is an armstrong number or not
-#include <iostream.h>
-#include <Math.h>
+#include <iostream>
+#include <math.h>
+
+using namespace std;
 int main()
 {
   int n, k, d, s = 0;
